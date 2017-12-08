ffa90f9a4416c1d84dd01713e0a8484a02585c34
K Pachtrachai <krittin.pachtrachai.13@ucl.ac.uk>
2017-12-08 17:12:56 +0000

    Change marker and trail colour

--- youbot_trail_rviz/src/main.cpp
+++ youbot_trail_rviz/src/main.cpp
@@ -10,27 +10,46 @@
 #include <boost/foreach.hpp>
 #define foreach BOOST_FOREACH
 
-visualization_msgs::Marker robot_trail;
-geometry_msgs::Point gaz_point;
-
+visualization_msgs::Marker reached_points, robot_trail, points;
+geometry_msgs::Point point_buffer;
+double d;
 void update_line(const gazebo_msgs::LinkStates::ConstPtr &pos)
 {
     int L = pos->pose.size();
 
-    gaz_point.x = 0.5*(pos->pose.at(L - 1).position.x + pos->pose.at(L - 2).position.x);
-    gaz_point.y = 0.5*(pos->pose.at(L - 1).position.y + pos->pose.at(L - 2).position.y);
-    gaz_point.z = 0.5*(pos->pose.at(L - 1).position.z + pos->pose.at(L - 2).position.z);
+    point_buffer.x = 0.5*(pos->pose.at(L - 1).position.x + pos->pose.at(L - 2).position.x);
+    point_buffer.y = 0.5*(pos->pose.at(L - 1).position.y + pos->pose.at(L - 2).position.y);
+    point_buffer.z = 0.5*(pos->pose.at(L - 1).position.z + pos->pose.at(L - 2).position.z);
 
-    if (robot_trail.points.size() < 4000)
+    if (robot_trail.points.size() < 800)
     {
-        robot_trail.points.push_back(gaz_point);
+        robot_trail.points.push_back(point_buffer);
     }
     else
     {
         robot_trail.points.erase(robot_trail.points.begin());
-        robot_trail.points.push_back(gaz_point);
+        robot_trail.points.push_back(point_buffer);
     }
 
+}
+
+void update_point()
+{
+    for (int i = 0; i < robot_trail.points.size(); i++)
+    {
+        for (int j = 0; j < points.points.size(); j++)
+        {
+            d = sqrt(pow(robot_trail.points.at(i).x - points.points.at(j).x, 2) +
+                             pow(robot_trail.points.at(i).y - points.points.at(j).y, 2) +
+                             pow(robot_trail.points.at(i).z - points.points.at(j).z, 2));
+            if (d < 0.018)
+            {
+                reached_points.points.push_back(points.points.at(j));
+                points.points.erase(points.points.begin() + j);
+                return;
+            }
+        }
+    }
 }
 
 int main( int argc, char** argv )
@@ -48,32 +67,45 @@ int main( int argc, char** argv )
     KDL::Frame current_pose;
 
     //Define points message
-    visualization_msgs::Marker points;
-    points.header.frame_id = robot_trail.header.frame_id = "/base_link";
-    points.header.stamp = robot_trail.header.stamp = ros::Time::now();
-    points.ns = robot_trail.ns = "points_and_lines";
-    points.action = robot_trail.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = robot_trail.pose.orientation.w =1.0;
+    points.header.frame_id = robot_trail.header.frame_id = reached_points.header.frame_id = "/base_link";
+    points.header.stamp = robot_trail.header.stamp = reached_points.header.stamp = ros::Time::now();
+    points.ns = robot_trail.ns = reached_points.ns = "points_and_lines";
+    points.action = robot_trail.action = reached_points.action = visualization_msgs::Marker::ADD;
+    points.pose.orientation.w = robot_trail.pose.orientation.w = reached_points.pose.orientation.w = 1.0;
 
     points.id = 0;
     robot_trail.id = 1;
+    reached_points.id = 2;
 
     points.type = visualization_msgs::Marker::POINTS;
+    reached_points.type = visualization_msgs::Marker::POINTS;
     robot_trail.type = visualization_msgs::Marker::LINE_STRIP;
 
     // POINTS markers use x and y scale for width/height respectively
     points.scale.x = 0.01;
     points.scale.y = 0.01;
 
-    // Points are green
-    points.color.g = 1.0f;
+    // Unreached points are red
+    points.color.r = 1.0f;
     points.color.a = 1.0;
 
 
+    // POINTS markers use x and y scale for width/height respectively
+    reached_points.scale.x = 0.01;
+    reached_points.scale.y = 0.01;
+
+    // Reached points are red
+    reached_points.color.g = 1.0f;
+    reached_points.color.a = 1.0;
+
+
+    //Define the width of robot trail
     robot_trail.scale.x = 0.005;
 
-    // Robot trails are blue
+    // Robot trails are white
+    robot_trail.color.r = 1.0f;
     robot_trail.color.g = 1.0f;
+    robot_trail.color.b = 1.0f;
     robot_trail.color.a = 1.0;
 
     rosbag::Bag bag;
@@ -162,6 +194,9 @@ int main( int argc, char** argv )
     {
         marker_pub.publish(points);
         marker_pub.publish(robot_trail);
+        marker_pub.publish(reached_points);
+
+        update_point();
 
         ros::spinOnce();
 
